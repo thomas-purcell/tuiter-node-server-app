833a688313766137d6bcd7b1ebfdd922d2c76c61
thomas-purcell <purcell.t@northeastern.edu>
2023-04-06 12:12:50 -0400

    update schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -4,6 +4,16 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  replies: Number,
+  retuits: Number,
+  dislikes: Number,
+  disliked: Boolean,
+  title: String,
+  time: String,
+  handle: String,
+  username: String,
+  image: String,
+  topic: String
 }, {collection: 'tuits'});
 
 export default schema;
